ef1b049af8a436285941b871597c236d05bab849
Ausaf Ahmed <ausaf.ahmed@hotmail.com>
2019-03-20 15:47:06 -0700

    WebApp Exception Handled

--- src/main/java/org/ausaf/dev/messenger/exception/DataNotFoundException.java
+++ src/main/java/org/ausaf/dev/messenger/exception/DataNotFoundException.java
@@ -1,7 +1,14 @@
 package org.ausaf.dev.messenger.exception;
 
 public class DataNotFoundException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 4668297249787122842L;
 	
-	
+	public DataNotFoundException(String message) {
+		super(message);
+	}
 
 }
--- src/main/java/org/ausaf/dev/messenger/service/CommentService.java
+++ src/main/java/org/ausaf/dev/messenger/service/CommentService.java
@@ -4,11 +4,14 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+import javax.ws.rs.NotFoundException;
 import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 
 import org.ausaf.dev.messenger.database.DatabaseClass;
 import org.ausaf.dev.messenger.model.Comment;
+import org.ausaf.dev.messenger.model.ErrorMessage;
 import org.ausaf.dev.messenger.model.Message;
 
 public class CommentService {
@@ -21,14 +24,18 @@ public class CommentService {
 	}
 	
 	public Comment getComment(long messageId, long commentId) {
+		ErrorMessage errorMessage = new ErrorMessage("Not found", 404, "Custom Documentation URL");
+		Response response = Response.status(Status.NOT_FOUND)
+				.entity(errorMessage)
+				.build();
 		Message message = messages.get(messageId);
 		if(message == null) {
-			throw new WebApplicationException(Status.NOT_FOUND);
+			throw new WebApplicationException(response);
 		}
 		Map<Long, Comment> comments =  messages.get(messageId).getComments();
 		Comment comment =  comments.get(commentId);
 		if(comment == null) {
-			throw new WebApplicationException(Status.NOT_FOUND);
+			throw new NotFoundException(response);
 		}
 		return comment;
 	}
